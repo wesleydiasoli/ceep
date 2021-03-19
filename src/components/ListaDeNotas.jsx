bfe2854240fa196913d745819c50f4fa0cd46fe7
Wesley Dias <wesleydias96@gmail.com>
2021-03-19 02:04:53 -0300

    commit

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
-import CardNota from "./CardNota";
+import CardNota from "./CardNotas/CardNota";
 
 class ListaDeNotas extends Component{
 
     render(){
-        return ( 
+        return (
             <ul>
-                <li>
-                <CardNota/>
-            </li>
+                {Array.of("Trabalho", "Trabalho", "Estudos", ).map((index) => {
+                    return (
+                        <li key = {index}>
+                            
+                            <CardNota/>
+                        </li>
+                    );
+                })}
             </ul>
-        );
-        
+        );    
     }
 }
 export default ListaDeNotas;
+
+/*O que aprendemos nessa aula:
+
+Como podemos criar class components
+
+Entendendo como o JSX renderiza objetos
+
+Hierarquia entre componentes (Pai/filhos)
+
+Componentes podem ter filhos que são componentes
+
+Como integrar o Javascript no meio do seu JSX */
